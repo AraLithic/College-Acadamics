0053dd5b559eeaa8626094b3aac6f1e3b7cbe143
AraLithic <aralithic@gmail.com>
2026-01-29 13:18:12 +0530

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,16 @@
+#strings
+first_name = "Sayed"
+food ="Pizza"
+email ="sayedumairali@outlook.com"
+print(f"Hello {first_name}")
+print(f"You like {food}")
+print(f"your email is {email}")
+#integers
+age=20
+print(f"your age is {age}")
+#float
+dick_size=5
+print(f"Dick size is {dick_size} centimeter")
+#boolean
+is_student=True
+print(f"is_student is {is_student}")
